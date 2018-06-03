6a2ebfb3d3d1960eec1dad7d3a34fe128cdead64
atinyakov <akoff16@gmail.com>
2018-06-03 18:01:58 +0300

    module1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+var WizardHeight;
+
+function getFireballSpeed(left) {
+  return left ?  5 : 2;
+}
+
+function getWizardHeight() {
+  return WizardHeight = 1.337 * wizardWidth;
+}
+
+function getWizardX(width) {
+  return (width - wizardWidth / 2) / 2;
+}
+
+function getWizardY(height) {
+  return (height - WizardHeight) * 2 / 3;
+}
